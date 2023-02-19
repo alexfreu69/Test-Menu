88519e5acb88fb25ea6e7b40249ba19fff35de96
Alexander Freudenberg <40298730+alexfreu69@users.noreply.github.com>
2023-02-19 10:44:02 +0100

    Add files via upload

---
+++ ReadFile.ps1
@@ -0,0 +1,31 @@
+﻿function Read1()
+{
+    for ($i=1;$i -lt 10;$i++){
+        Get-Content -LiteralPath c:\temp\mappe1.xls -Encoding Byte
+    }
+}
+
+function Read2()
+{
+    for ($i=1;$i -lt 10;$i++){
+        [System.IO.File]::ReadAllBytes("c:\temp\mappe1.xls")
+    }
+}
+
+
+#(Measure-Command {Read1}).Milliseconds
+#(Measure-Command {Read2}).Milliseconds
+
+function Test-Menu($filename)
+{
+    $ret = $false
+    [byte[]] $b = [System.IO.File]::ReadAllBytes($filename)
+    [byte[]] $SIG = 0xD0,0xCF,0x11,0xE0,0xA1,0xB1,0x1A,0xE1
+    [byte[]] $x=$b[0..7]
+    Write-Host ([Linq.Enumerable]:: SequenceEqual($x, $SIG))
+    Write-Host $x
+    Write-Host $Sig
+    return $ret
+}
+
+Test-Menu "c:\temp\mappe1.xls"
